1682ee8a49ce78c1b761d8ae22f273c69800ec4e
Richard <1625351+richardzone@users.noreply.github.com>
2024-05-16 03:26:29 +0800

    [feat] add calibration_required and calibration_done commands

--- app/main.py
+++ app/main.py
@@ -1,10 +1,34 @@
 import time
 import random
-from typing import Tuple
+from typing import Tuple, List
 import argparse
-
 import serial
+from abc import ABC, abstractmethod
 
+class Command(ABC):
+    @abstractmethod
+    def get_command(self) -> str:
+        pass
+
+class RandomCoordinatesCommand(Command):
+    def __init__(self, screen_width: int, screen_height: int):
+        self.screen_width = screen_width
+        self.screen_height = screen_height
+
+    def generate_random_coordinates(self) -> Tuple[int, int]:
+        return random.randint(0, self.screen_width), random.randint(0, self.screen_height)
+
+    def get_command(self) -> str:
+        coordinates = self.generate_random_coordinates()
+        return str(coordinates)
+
+class CalibrationRequiredCommand(Command):
+    def get_command(self) -> str:
+        return "calibration_required"
+
+class CalibrationDoneCommand(Command):
+    def get_command(self) -> str:
+        return "calibration_done"
 
 def get_screen_size() -> Tuple[int, int]:
     # Using tkinter to get screen size
@@ -15,28 +39,30 @@ def get_screen_size() -> Tuple[int, int]:
     root.destroy()
     return width, height
 
-
-def generate_random_coordinates(screen_width: int, screen_height: int) -> Tuple[int, int]:
-    return [random.randint(0, screen_width), random.randint(0, screen_height)]
-
-
 def main():
-
     parser = argparse.ArgumentParser(description='Serial communication emulator')
     parser.add_argument('--port', default='COM2', help='Serial port (default: COM2)')
+    parser.add_argument('--rate', type=int, default=9600, help='Baud rate (default: 9600)')
     args, _ = parser.parse_known_args()
     port = args.port
+    rate = args.rate
 
     screen_width, screen_height = get_screen_size()
-    with serial.Serial(port, 9600, timeout=0) as ser:
-        while True:
-            coordinates = list(generate_random_coordinates(screen_width, screen_height))
-            encoded_text = str(coordinates).encode()
-            ser.write(str(coordinates).encode())
-            ser.flush()
-            print(f"Sent to {port}: {encoded_text}\n")
-            time.sleep(random.uniform(0.5, 4))
+    commands: List[Command] = [
+        RandomCoordinatesCommand(screen_width, screen_height),
+        CalibrationRequiredCommand(),
+        CalibrationDoneCommand()
+    ]
 
+    with serial.Serial(port, rate, timeout=0) as ser:
+        while True:
+            command = random.choice(commands)
+            command_str = command.get_command()
+            ser.write(command_str.encode())
+            # ser.flush()
+            sleep_duration = random.uniform(0.5, 4)
+            print(f"Sent to {port}: {command_str}. Now sleeping for {sleep_duration:.2f} seconds\n")
+            time.sleep(sleep_duration)
 
 if __name__ == '__main__':
     main()
